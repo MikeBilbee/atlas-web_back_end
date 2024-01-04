0eb51bd435bf698c33b57dd777278561c6046c2a
MikeBilbee <cmbilbee@gmail.com>
2024-01-04 15:25:51 -0600

    2.0 courses

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,39 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+
+    
+    
+  }
+
+  get name() {
+    return this._name;
+  }
+  get length() {
+    return this._length;
+  }
+  get students() {
+    return this._students;
+  }
+
+  set name(newN) {
+    if (typeof newN !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newN
+  }
+  set length(newL) {
+    if (typeof newL !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newL
+  }
+  set students(newS) {
+    if (!Array.isArray(newS) || !newS.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = newS
+  }
+}
