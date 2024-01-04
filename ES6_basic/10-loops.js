0683893a1405b17cacead9c387ede406468d2e8e
MikeBilbee <cmbilbee@gmail.com>
2024-01-03 18:29:25 -0600

    linter not happy so I went back one step

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const value of array) {
-    array[array.indexOf(value)] = appendString + value;
+    const index = array.indexOf(value);
+    array[index] = appendString + value;
   }
 
   return array;
