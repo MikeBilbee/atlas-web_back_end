29106193f1d2b312e8277791ed916b12701ed941
MikeBilbee <cmbilbee@gmail.com>
2024-01-05 14:41:09 -0600

    4.0 static methods

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(nAmount) {
+    this._amount = nAmount;
+  }
+
+  set currency(nCurrency) {
+    this._currency = nCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion rate must be a number');
+    }
+
+    return amount * conversionRate;
+  }
+}
