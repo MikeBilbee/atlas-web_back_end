308f4caa406d045803d0365cc1316217f69f8f60
MikeBilbee <cmbilbee@gmail.com>
2024-01-08 13:27:11 -0600

    0.2 reworking.2

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 export default function getListStudents() {
   const students = [
-    { 'id': 1, 'firstName': 'Guillaume', 'location': 'San Francisco'},
-    { 'id': 2, 'firstName': 'James', 'location': 'Colorado'},
-    { 'id': 3, 'firstName': 'Serena', 'location': 'San Francisco'},
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Colorado' },
+    { id: 3, firstName: 'Serena', location: 'San Francisco' },
   ];
 
-return students;
+  return students;
 }
