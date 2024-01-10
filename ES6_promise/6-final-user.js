5d365aec1304d037712cdb237af0f4dce30aa924
MikeBilbee <cmbilbee@gmail.com>
2024-01-10 13:57:51 -0600

    6.3 trying this

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -7,7 +7,13 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
       signUpUser(firstName, lastName),
       uploadPhoto(fileName),
     ])
-      .then(async (myStatus) => ({ status: 'fulfilled', value: myStatus }))
-      .catch((error) => ({ status: 'rejected', value: error.toString() })),
+      .then(async (myStatus) => ({
+        status: 'fulfilled',
+        value: myStatus,
+      }))
+      .catch((error) => ({
+        status: 'rejected',
+        value: error.toString(),
+      })),
   ];
 }
