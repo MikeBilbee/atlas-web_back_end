0f86d754949a40b7779ecb48d75bea22f88fdc23
MikeBilbee <cmbilbee@gmail.com>
2024-01-10 13:44:28 -0600

    6.0 await and async

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return [
+    await Promise.allSettled([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
+    ])
+      .then((myStatus) => ({ status: 'resolved', value: myStatus }))
+      .catch((error) => ({ status: 'rejected', value: error.toString() })),
+  ];
+}
