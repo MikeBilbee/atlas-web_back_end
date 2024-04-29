16438722f3d9c5e63b7a2381c55faa03ec6febf3
MikeBilbee <cmbilbee@gmail.com>
2024-04-29 18:44:15 -0500

    setup and 0.0 basic async syntax

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+import asyncio
+import random
+import importlib
+basic_async_syntax = importlib.import_module("0-basic_async_syntax")
+wait_random = basic_async_syntax.wait_random
