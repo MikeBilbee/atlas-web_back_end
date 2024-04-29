e4ab1e3bfa2746e68acf5130a143ff178ded1e27
MikeBilbee <cmbilbee@gmail.com>
2024-04-29 16:41:39 -0500

    is this documented enough?

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-""" Adds 2 digits then returns sum as float.  """
-
-
 def add(a: float, b: float) -> float:
+    """ Adds 2 digits then returns sum as float.  """
     return a + b
