7ec977a0c5b89f3ab770af7291711e352a4123c8
MikeBilbee <cmbilbee@gmail.com>
2024-05-05 18:08:36 -0500

    2.0 measure runtime

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+Import async_comprehension from the previous
+file and write a measure_runtime coroutine that
+will execute async_comprehension four times in
+parallel using asyncio.gather.
+"""
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+
+    """Measures runtime of itself"""
+
+    start_time: float = time.time()
+    funcs = [asyncio.create_task(async_comprehension()) for i in range(4)]
+    await asyncio.gather(*funcs)
+    end_time: float = time.time()
+    return end_time - start_time
