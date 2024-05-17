1f99f5a9a97bc6ddcbdf6b1d81472a85753fbf63
MikeBilbee <cmbilbee@gmail.com>
2024-05-17 11:32:05 -0500

    3.0 deletion pagination

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -5,7 +5,7 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import List
+from typing import List, Dict
 
 
 class Server:
@@ -40,4 +40,35 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            pass
+        """
+        A get_hyper_index method with two integer arguments: index with a None
+        default value and page_size with default value of 10.
+        """
+
+        assert index is None or (
+            type(index) == int and index >= 0
+        )
+        assert type(page_size) == int and page_size > 0
+
+        indexed_dataset = self.indexed_dataset()
+        total_items = len(indexed_dataset)
+        data = []
+
+        if index is None:
+            index = 0
+        else:
+            assert index < total_items
+
+        next_index = index + page_size
+
+        while index < total_items and len(data) < page_size:
+            if index in indexed_dataset:
+                data.append(indexed_dataset[index])
+            index += 1
+
+        return {
+            "index": index,
+            "next_index": next_index if next_index < total_items else None,
+            "page_size": page_size,
+            "data": data,
+        }
