affb8225a0ea53f59413221ad79b747996b023d4
MikeBilbee <cmbilbee@gmail.com>
2024-05-20 12:11:17 -0500

    6.0 check password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -16,3 +16,9 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """An is_valid function that expects 2 arguments and returns a boolean."""
+
+    return bcrypt.checkpw(password.encode(), hashed_password)
