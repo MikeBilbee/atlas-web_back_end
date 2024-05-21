3fbf6b12b2ec9e4ec975d6c83322e9c4db422442
MikeBilbee <cmbilbee@gmail.com>
2024-05-21 10:44:15 -0500

    6.0 Basic Auth

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/bin/env python3
+"""
+Basic Authorization for the API server
+"""
+
+from api.v1.auth.auth import Auth, TypeVar
+
+
+class BasicAuth(Auth):
+    """Basic Authorizations Class, Inheriting from Auth"""
+
+    pass
