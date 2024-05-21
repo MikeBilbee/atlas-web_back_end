73b5c94f394be634b11ec76b133b131fb8677332
MikeBilbee <cmbilbee@gmail.com>
2024-05-21 11:15:36 -0500

    8.0 Base64

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,9 @@
 Basic Authorization for the API server
 """
 
-from api.v1.auth.auth import Auth, TypeVar
+from api.v1.auth.auth import Auth
+import base64
+from typing import Tuple, TypeVar
 
 
 class BasicAuth(Auth):
@@ -19,3 +21,16 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """Returns the decoded value of a Base64 string"""
+
+        if base64_authorization_header is None or not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            return decoded.decode('utf-8')
+        except base64.binascii.Error:
+            return None
