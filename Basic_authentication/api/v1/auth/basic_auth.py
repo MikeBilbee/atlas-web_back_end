6ffe2d5da89721da1f17a446c042bbbaf80f070d
MikeBilbee <cmbilbee@gmail.com>
2024-05-21 10:59:36 -0500

    7.1 # noqa to the rescue

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,10 +9,10 @@ from api.v1.auth.auth import Auth, TypeVar
 class BasicAuth(Auth):
     """Basic Authorizations Class, Inheriting from Auth"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:  # noqa E501
         """Gets the Base64 section of the Auth Header"""
 
-        if authorization_header is None or not isinstance(authorization_header, str):
+        if authorization_header is None or not isinstance(authorization_header, str):  # noqa E501
             return None
 
         if not authorization_header.startswith("Basic "):
