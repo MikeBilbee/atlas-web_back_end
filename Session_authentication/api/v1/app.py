e69c75dd961d60258545e02c47522546f76793de
MikeBilbee <cmbilbee@gmail.com>
2024-05-24 12:42:33 -0500

    5.0 idk what the fuck is happening here

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -53,7 +53,17 @@ def before_request() -> str:
     """
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/'
+                      '/api/v1/auth_session/login/']
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    request.current_user = auth.current_user(request)
+
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+        abort(401)
+
     if auth is None or request.path in excluded_paths:
         return
 
