7f89bf891eab2155c2791191d3cc5c96eb8371ee
MikeBilbee <cmbilbee@gmail.com>
2024-05-24 13:08:01 -0500

    7.1 ahhhh

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,13 +3,9 @@
 WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 """
 
-from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from models.user import User
 import uuid
-from flask import jsonify, request, abort, make_response
-from models.user import User
-import os
 
 
 class SessionAuth(Auth):
@@ -49,33 +45,3 @@ class SessionAuth(Auth):
             return None
         user = self.user_id_for_session_id(cookie_value)
         return User.get(user)
-
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login():
-    """Handles user login using session authentication."""
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    if not email:
-        return jsonify({"error": "email missing"}), 400
-
-    if not password:
-        return jsonify({"error": "password missing"}), 400
-
-    user_list = User.search(email=email)
-    if not user_list:
-        return jsonify({"error": "no user found for this email"}), 404
-
-    user = user_list[0]
-
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-
-    from api.v1.app import auth
-
-    session_id = auth.create_session(user.id)
-    response = make_response(jsonify(user.to_dict()))
-    response.set_cookie(
-        os.getenv('SESSION_NAME'), session_id
-    )
-    return response, 200
---
+++ Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,41 @@
+#!/bin/usr/env python3
+"""View route for session auth"""
+
+from flask import Blueprint
+from api.v1.views import app_views
+from flask import jsonify, request, make_response
+import os
+from models.user import User
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """Handles user login using session authentication."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    user_list = User.search(email=email)
+    if not user_list:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = user_list[0]
+
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+
+    session_id = auth.create_session(user.id)
+    response = make_response(jsonify(user.to_dict()))
+    response.set_cookie(
+        os.getenv('SESSION_NAME'), session_id
+    )
+    return response, 200
