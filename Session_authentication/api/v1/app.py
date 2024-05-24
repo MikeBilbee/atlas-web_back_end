e1c8e0631a1e0579abdc79a175dab8737edb1b07
MikeBilbee <cmbilbee@gmail.com>
2024-05-24 12:45:29 -0500

    5.1 hmmm

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -49,33 +49,20 @@ def forbidden(error) -> str:
 
 @app.before_request
 def before_request() -> str:
-    """Handles requests before they are processed.
-    """
+    """ method to handler before request """
+    if auth is None:
+        return
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/'
+                      '/api/v1/forbidden/',
                       '/api/v1/auth_session/login/']
-
     if not auth.require_auth(request.path, excluded_paths):
         return
-
-    request.current_user = auth.current_user(request)
-
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+    if not auth.authorization_header(request)\
+       and not auth.session_cookie(request):
         abort(401)
-
-    if auth is None or request.path in excluded_paths:
-        return
-
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-
-    if auth.authorization_header(request) is None:
-        abort(401)
-
     if auth.current_user(request) is None:
         abort(403)
-
     request.current_user = auth.current_user(request)
 
 
