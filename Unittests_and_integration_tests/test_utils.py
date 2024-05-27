b246911777a04dacc209343a124527718ca9adfc
MikeBilbee <cmbilbee@gmail.com>
2024-05-27 14:23:01 -0500

    0.1 im not sure what its mad at

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,7 @@ Test Suite for Utils, Client, & Fixtures
 import unittest
 import parameterized
 from utils import access_nested_map
+from typing import Mapping, Sequence, Any
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -16,11 +17,13 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(self,
+                               nested_map: Mapping,
+                               path: Sequence,
+                               expected: Any):
         """Test for AccessNestedMap"""
 
-        result = access_nested_map(nested_map, path)
-        self.assertEqual(result, expected)
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
 if __name__ == '__main__':
     unittest.main()
