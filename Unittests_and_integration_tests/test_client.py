022cbcd4e41a64bc0608e72d0ee9772450f763b5
MikeBilbee <cmbilbee@gmail.com>
2024-05-28 13:32:50 -0500

    4.0 test client

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Test suite for Client"""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from typing import Mapping, Sequence, Any, Dict, Callable
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test class for GithubOrgClient"""
+
+    @parameterized.expand([
+        ("google")
+        ("abc")
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name: str):
+        """Test for GithubOrgClient"""
+
+        client = GithubOrgClient(org_name)
+
+        first_response = client.org
+
+        second_response = client.org
+
+        self.assertIs(first_response, second_response) 
+
+if __name__ == '__main__':
+    unittest.main()
