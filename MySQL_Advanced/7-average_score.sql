ceafaf496ec7a4d462d33faaa401eea5bfbe3218
MikeBilbee <cmbilbee@gmail.com>
2024-06-10 14:38:49 -0500

    7.1 Avg score

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -8,14 +8,10 @@ BEGIN
     DECLARE avg_score DECIMAL(10, 2);
 
     -- Calculate the average score for the given user
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
+    SELECT AVG(score) INTO average_score FROM corrections WHERE corrections.user_id = user_id;
 
     -- Update the user's average score
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
+    UPDATE users SET average_score = average_score WHERE id = user_id;
 END//
 
 DELIMITER ;
