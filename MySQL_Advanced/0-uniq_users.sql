202bbeff3acbe8395f4cd6c6c960f32cb5212206
MikeBilbee <cmbilbee@gmail.com>
2024-06-10 13:01:52 -0500

    0.1 aaauggh

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,10 +1,6 @@
 -- Check if the table already exists
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'users')
-BEGIN
-    -- Create the table if it doesn't exist
-    CREATE TABLE users (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        email VARCHAR(255) NOT NULL UNIQUE,
-        name VARCHAR(255)
-    );
-END;
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
