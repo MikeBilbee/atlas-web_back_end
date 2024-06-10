7320790bd10bab7536925c92edcdda05e9b52999
MikeBilbee <cmbilbee@gmail.com>
2024-06-10 15:44:36 -0500

    11.0 No Table

---
+++ MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- A view of Students who have below an 80 and haven't had a meeting yet
+CREATE VIEW need_meeting AS
+SELECT student_name
+FROM students
+WHERE score < 80
+AND (last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH))
