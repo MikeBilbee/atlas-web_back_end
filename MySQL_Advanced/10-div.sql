8c83e6fd3914c3812722c442b8d9a4deb5931f92
MikeBilbee <cmbilbee@gmail.com>
2024-06-10 15:39:19 -0500

    10.2 division

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,13 +1,10 @@
 -- Divides 2 numbers and returns the result or 0
 DELIMITER //
 
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS FLOAT DETERMINISTIC
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 BEGIN
-	--checks if second number is 0
     IF b = 0 THEN
 		RETURN 0;
-	--does division
     ELSE
         RETURN a / b;
     END IF;
