4eb9bf8366bf0594d2cc95781dc3a0aea523fb8e
MikeBilbee <cmbilbee@gmail.com>
2024-06-10 14:47:20 -0500

    8.0 new index

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -8,7 +8,7 @@ BEGIN
     DECLARE avg_score DECIMAL(10, 2);
 
     -- Calculate the average score for the given user
-    SET avg_score = (SELECT AVG(score) FROM corrections as c WHERE c.user_id=user_id);
+    SET avg_score = (SELECT AVG(score) FROM corrections as c WHERE c.user_id = user_id);
 
     -- Update the user's average score
     UPDATE users SET average_score = avg_score WHERE id = user_id;
---
+++ MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,2 @@
+-- Creates an index of first initials
+CREATE INDEX idx_name ON names (name(1));
