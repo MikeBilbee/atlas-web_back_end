283b2484c1c2ea7ef5d91c229cd65cf0a66e13f3
MikeBilbee <cmbilbee@gmail.com>
2024-06-10 13:19:20 -0500

    3.0 ow

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- List Glam rock bands ranked by band lifespan
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
