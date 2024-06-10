4697a6d121f4aa5922ddae517a72c9e82a5e7755
MikeBilbee <cmbilbee@gmail.com>
2024-06-10 14:13:39 -0500

    5.0 new email

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- Resets valid_email when email is updated
+DELIMITER //
+
+CREATE TRIGGER before_email_update
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email <> OLD.email THEN
+		SET NEW.valid_email = FALSE
+	END IF;
+END
+//
+
+DELIMITER;
