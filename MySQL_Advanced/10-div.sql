810718e2a4703686a374e045a6c3f61f8f793e83
MikeBilbee <cmbilbee@gmail.com>
2024-06-10 15:36:16 -0500

    10.1 division

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -2,20 +2,18 @@
 DELIMITER //
 
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
+RETURNS FLOAT DETERMINISTIC
 BEGIN
     DECLARE result INT;
 
 	--checks if second number is 0
     IF b = 0 THEN
-        SET result = 0;
-
+	RETURN 0;
 	--does division
     ELSE
-        SET result = a / b;
+        RETURN a / b;
     END IF;
 
-    RETURN result;
 END //
 
 DELIMITER ;
