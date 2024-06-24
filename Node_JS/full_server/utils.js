e35ff552fa8b73a44fd7693de6398bd518c30ad5
MikeBilbee <cmbilbee@gmail.com>
2024-06-24 14:12:49 -0500

    8-1.0 setup and utils

---
+++ Node_JS/full_server/utils.js
@@ -0,0 +1,16 @@
+// 
+
+const fs = require('fs');
+
+async function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {
+    try {
+      const data = fs.readFile(filePath, 'utf8');
+      resolve(data);
+    } catch (error) {
+      reject(new Error(error));
+    }
+  });
+}
+
+module.exports = readDatabase;
