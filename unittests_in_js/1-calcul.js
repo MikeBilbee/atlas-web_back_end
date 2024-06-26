143e0f2f144097a37bf8b9e1f7287cb08f24ad4c
MikeBilbee <cmbilbee@gmail.com>
2024-06-26 13:29:14 -0500

    0.1 calculator

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -2,9 +2,8 @@
 // and returns the sum.
 
 function calculateNumber(a, b) {
-  const roundedA = Math.round(a);
-  const roundedB = Math.round(b);
-  return roundedA + roundedB;
+  // Rounds two numbers and returns the sum
+  return Math.round(a) + Math.round(b);
 }
 
 module.exports = calculateNumber;
