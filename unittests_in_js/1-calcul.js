2e83d806c737d079daaff7d402751dbc51cd4882
MikeBilbee <cmbilbee@gmail.com>
2024-06-26 15:20:36 -0500

    1.0 calculator

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -1,7 +1,18 @@
 // A function that accepts two arguments, rounds them,
-// and returns the sum.
+// and returns the sum, differnce, or quotient
 
-export default function calculateNumber(a, b) {
-  // Rounds two numbers and returns the sum
-  return Math.round(a) + Math.round(b);
+function calculateNumber(type, a, b) {
+  // Rounds two numbers and returns the sum, difference, or division
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  } else if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  } else if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) {
+      return 'Error';
+    }
+    return Math.round(a) / Math.round(b);
+  }
 }
+
+module.exports = calculateNumber;
---
+++ unittests_in_js/1-calcul.test.js
@@ -0,0 +1,26 @@
+// Tests for 1-calcul.js
+
+const calculateNumber = require('./1-calcul');
+const assert = require('assert');
+
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return rounded sum for SUM', function() {
+      assert.strictEqual(calculateNumber('SUM', 6.2, 11.8), 18);
+    });
+  });
+  describe('SUBTRACT', () => {
+    it('should return rounded difference for SUBTRACT', function() {
+      assert.strictEqual(calculateNumber('SUBTRACT', 15.8, 6.2), 10);
+    });
+  });
+  describe('DIVIDE', () => {
+    it('should return rounded division for DIVIDE', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 9.3, 2.7), 3);
+    });
+    it('should return Error for DIVIDE when b is 0', function() {
+      assert.strictEqual(calculateNumber('DIVIDE', 56.9, 0), 'Error');
+    });
+  });
+});
