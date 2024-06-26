ef2b0915096774923950edd5ac5da98a7ba41ea2
MikeBilbee <cmbilbee@gmail.com>
2024-06-26 14:18:52 -0500

    0.3 omg finally

---
+++ unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+// A function that accepts two arguments, rounds them,
+// and returns the sum.
+
+function calculateNumber(a, b) {
+  // Rounds two numbers and returns the sum
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
