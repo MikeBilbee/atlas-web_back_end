d3bfebeb840d89201d8e98d3c70fb971bab7f4f2
MikeBilbee <cmbilbee@gmail.com>
2024-06-26 17:54:26 -0500

    9.0 whee

---
+++ unittests_in_js/9-api/api.test.js
@@ -0,0 +1,34 @@
+// Tests for 9-api/api.js
+
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index page', function () {
+  it('should return the correct status code and result', function (done) {
+    request('http://localhost:7865', function (error, response, body) {
+      if (error) return done(error);
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', function () {
+  it('should return the correct status code and result', function (done) {
+    request('http://localhost:7865/cart/12', function (error, response, body) {
+      if (error) return done(error);
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal(`Payment methods for cart 12`);
+      done();
+    });
+  });
+
+  it('should return the correct status code and result', function (done) {
+    request('http://localhost:7865/cart/test', function (error, response, body) {
+      if (error) return done(error);
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
